7d5e47f787c147c5008ac71f0efb443e7940a783
paulohenriquepm <paulo.henriquepm@outlook.com>
2020-08-31 21:42:49 -0300

    feat: add eslint and prettier

---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+    react: {
+      version: 'detect',
+    },
+  }
+}
