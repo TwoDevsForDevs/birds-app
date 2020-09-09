23d8a2ad6a91f3ef12cc1d9ae4c1248c3b89b951
Matheus Pires <matheushenriquepires99@gmail.com>
2020-09-08 21:19:07 -0300

    chore: create theme file

---
+++ packages/mobile/src/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: '#272727',
+    primaryText: '#272727',
+    secundaryText: '#86868A',
+    background: '#FBFBFB',
+    white: '#FFFFFF',
+    backgroundModalOpen: 'rgba(0, 0, 0, 0.3)',
+    inputBackground: '#EEEEEE',
+    error: '#E74C3C'
+  },
+  radius: {
+    default: '8px',
+    small: '4px'
+  },
+  shadows: {
+    default: '0 2px 2px rgba(0, 0, 0, 0.1)'
+  }
+} as const;
