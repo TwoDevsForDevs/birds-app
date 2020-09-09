8b9513639410887d62c600e57f0f852ea4c48edb
Matheus Pires <matheushenriquepires99@gmail.com>
2020-09-08 21:19:22 -0300

    feat: use theme file and expo-google-fonts

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,11 +1,32 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { AppLoading } from 'expo';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
+
+import theme from './src/styles/theme';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </ThemeProvider>
   );
 };
 
