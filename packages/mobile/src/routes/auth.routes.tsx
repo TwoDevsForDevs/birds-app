511d9bd4878b17769cdd9ab70725964aac3428ff
Matheus Pires <matheushenriquepires99@gmail.com>
2020-09-10 22:54:15 -0300

    style: add default background color to auth routes

--- packages/mobile/src/routes/auth.routes.tsx
+++ packages/mobile/src/routes/auth.routes.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useTheme } from 'styled-components';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
 
-const AuthRoute: React.FC = () => (
-  <Auth.Navigator screenOptions={{ headerShown: false }}>
-    <Auth.Screen name="SignIn" component={SignIn} />
-    <Auth.Screen name="SignUp" component={SignUp} />
-  </Auth.Navigator>
-);
+const AuthRoute: React.FC = () => {
+  const { colors } = useTheme();
+
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: colors.background
+        }
+      }}
+    >
+      <Auth.Screen name="SignIn" component={SignIn} />
+      <Auth.Screen name="SignUp" component={SignUp} />
+    </Auth.Navigator>
+  );
+};
 
 export default AuthRoute;
