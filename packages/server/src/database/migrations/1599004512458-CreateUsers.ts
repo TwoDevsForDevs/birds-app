e7793319999a23675236a8824031b93888758551
paulohenriquepm <paulo.henriquepm@outlook.com>
2020-09-22 20:16:10 -0300

    refactor: add status column

--- packages/server/src/database/migrations/1599004512458-CreateUsers.ts
+++ packages/server/src/database/migrations/1599004512458-CreateUsers.ts
@@ -32,6 +32,11 @@ export default class CreateUsers1599004512458 implements MigrationInterface {
             isNullable: true
           },
           {
+            name: 'status',
+            type: 'boolean',
+            default: 1
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()'
