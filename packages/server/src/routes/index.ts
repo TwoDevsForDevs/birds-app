ac6c344e84cdb2ae06ce0a0f2c3716fcb81ef781
paulohenriquepm <paulo.henriquepm@outlook.com>
2020-09-22 20:34:49 -0300

    feat: use new Routers

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
-import usersRouter from './users.routes';
-import sessionsRouter from './sessions.routes';
+import usersRouter from '../modules/users/routes/users.routes';
+import sessionsRouter from '../modules/users/routes/sessions.routes';
+import birdsRouter from '../modules/birds/routes/birds.routes';
+import birdsRegisterRouter from '../modules/birdsRegisters/routes/birdsRegisters.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/birds', birdsRouter);
+routes.use('/birds-registers', birdsRegisterRouter);
 
 export default routes;
