2cbcc4d95b0b5046ca596ee7468b3eab6b807567
Matheus Pires <matheushenriquepires99@gmail.com>
2020-10-24 11:14:34 -0300

    feat: create ShowBirdService and show route

--- packages/server/src/modules/birds/controllers/BirdController.ts
+++ packages/server/src/modules/birds/controllers/BirdController.ts
@@ -3,6 +3,7 @@ import { classToClass } from 'class-transformer';
 
 import CreateBirdService from '../services/CreateBirdService';
 import ListBirdsService from '../services/ListBirdsService';
+import ShowBirdService from '../services/ShowBirdService';
 
 export default class BirdController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -11,6 +12,18 @@ export default class BirdController {
     const birds = await listBirdsService.execute();
 
     return response.json(classToClass(birds));
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id: bird_id } = request.params;
+
+    const showBirdService = new ShowBirdService();
+
+    const bird = await showBirdService.execute({
+      bird_id
+    });
+
+    return response.json(classToClass(bird));
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
---
+++ packages/server/src/modules/birds/services/ShowBirdService.ts
@@ -0,0 +1,24 @@
+import { getRepository } from 'typeorm';
+
+import Bird from '../entities/Bird';
+
+interface IRequest {
+  bird_id: string;
+}
+
+class ShowBirdService {
+  async execute({ bird_id }: IRequest): Promise<Bird | undefined> {
+    const birdRepository = getRepository(Bird);
+
+    const bird = birdRepository.findOne({
+      where: {
+        id: bird_id,
+        status: true
+      }
+    });
+
+    return bird;
+  }
+}
+
+export default ShowBirdService;
