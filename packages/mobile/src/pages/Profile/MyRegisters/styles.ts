2ab21b25ea31bc64956c1071ce11f95bcb4176e0
paulohenriquepm <paulo.henriquepm@outlook.com>
2020-10-28 10:42:04 -0300

    feat: finish MyRegisters Tab.Screen

---
+++ packages/mobile/src/pages/Profile/MyRegisters/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0px 24px 24px 24px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const BirdImage = styled.View`
+  margin-bottom: 16px;
+`;
