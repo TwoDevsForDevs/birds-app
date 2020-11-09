4fcdc2b5f17f3bc278d959c9bc2c204cc6c24744
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-09 15:48:44 -0300

    feat: create bird modal component

--- packages/mobile/src/components/BirdModal/index.tsx
+++ packages/mobile/src/components/BirdModal/index.tsx
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import Modal from 'react-native-modal';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
-import api from '../../services/api';
-
-import Placeholder from './Placeholder';
-
-import { Container, Content, BirdImage } from './styles';
+import {
+  Container,
+  Content,
+  CloseModalButton,
+  BirdImage,
+  StatsContainer,
+  StatsContent,
+  StatsNumber,
+  StatsText,
+  StatsDivider,
+  LikeButton,
+  LikeButtonText,
+  Divider,
+  BirdInfoContainer,
+  BirdInfo,
+  Label,
+  InfoText
+} from './styles';
 
 interface BirdRegister {
   id: string;
@@ -20,52 +35,77 @@ interface BirdRegister {
 }
 
 interface BirdModalProps {
-  birdId: string;
+  register: BirdRegister;
+  isVisible: boolean;
+  toggleModal: () => void;
 }
 
-const BirdModal: React.FC<BirdModalProps> = ({ birdId }) => {
-  const [register, setRegister] = useState<BirdRegister>({} as BirdRegister);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    async function loadRegister() {
-      try {
-        setLoading(true);
-
-        const response = await api.get(`birds-registers/${birdId}`);
-
-        setRegister(response.data);
-      } catch {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    loadRegister();
-  }, [birdId]);
-
-  if (loading) {
-    return <Placeholder />;
-  }
-
-  if (error) {
-    return (
-      <Container>
-        <Text>Error</Text>
-      </Container>
-    );
-  }
+const BirdModal: React.FC<BirdModalProps> = ({
+  isVisible,
+  register,
+  toggleModal
+}) => {
+  const { colors } = useTheme();
 
   return (
-    <Container>
-      {register && (
+    <Modal isVisible={isVisible} propagateSwipe onBackdropPress={toggleModal}>
+      <Container>
         <Content>
+          <CloseModalButton onPress={toggleModal}>
+            <Icon name="x" size={16} color={colors.white} />
+          </CloseModalButton>
+
           <BirdImage source={{ uri: register.image_url }} />
+
+          <StatsContainer>
+            <StatsContent>
+              <StatsNumber>250</StatsNumber>
+              <StatsText>curtidas</StatsText>
+            </StatsContent>
+
+            <StatsDivider>-</StatsDivider>
+
+            <StatsContent>
+              <StatsNumber>158</StatsNumber>
+              <StatsText>visualizações</StatsText>
+            </StatsContent>
+          </StatsContainer>
+
+          <LikeButton>
+            <LikeButtonText>Curtir</LikeButtonText>
+          </LikeButton>
+
+          <Divider />
+
+          <BirdInfoContainer>
+            <BirdInfo>
+              <Label>Autor:</Label>
+              <InfoText>Matheus</InfoText>
+            </BirdInfo>
+
+            <BirdInfo>
+              <Label>Localidate:</Label>
+              <InfoText>Rua das Palmeiras, 125</InfoText>
+            </BirdInfo>
+
+            <BirdInfo>
+              <Label>Data de registro:</Label>
+              <InfoText>13/08/2020</InfoText>
+            </BirdInfo>
+
+            <BirdInfo>
+              <Label>Observações:</Label>
+              <InfoText>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellat, aspernatur eos amet similique blanditiis tenetur
+                nulla, error neque rerum, perferendis impedit ab eum dolore
+                laborum accusamus iste beatae quas eveniet?
+              </InfoText>
+            </BirdInfo>
+          </BirdInfoContainer>
         </Content>
-      )}
-    </Container>
+      </Container>
+    </Modal>
   );
 };
 
--- packages/mobile/src/components/BirdModal/styles.ts
+++ packages/mobile/src/components/BirdModal/styles.ts
@@ -1,16 +1,93 @@
 import styled from 'styled-components/native';
-import { Modal } from 'react-native';
 
-export const Container = styled(Modal)``;
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false
+})`
+  flex: 0.9;
+  background: ${({ theme }) => theme.colors.white};
+  border-radius: ${({ theme }) => theme.radius.default};
+`;
 
 export const Content = styled.View`
   padding: 32px;
+`;
+
+export const CloseModalButton = styled.TouchableOpacity`
+  width: 32px;
+  height: 32px;
+  background: ${({ theme }) => theme.colors.grey};
+  border-radius: 20px;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  z-index: 2;
+
   align-items: center;
+  justify-content: center;
 `;
 
 export const BirdImage = styled.Image`
-  width: 264px;
-  height: 264px;
+  width: 100%;
+  height: 304px;
   border-radius: ${({ theme }) => theme.radius.default};
   margin-bottom: 16px;
+
+  align-self: center;
+`;
+
+export const StatsContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const StatsContent = styled.View`
+  flex-direction: row;
+`;
+
+export const StatsNumber = styled.Text`
+  font-family: 'Roboto_400Regular';
+  color: ${({ theme }) => theme.colors.grey};
+  margin-right: 4px;
+`;
+
+export const StatsText = styled.Text`
+  font-family: 'Roboto_400Regular';
+  color: ${({ theme }) => theme.colors.grey};
+`;
+
+export const StatsDivider = styled.Text`
+  font-family: 'Roboto_400Regular';
+  margin: 0 8px;
+  color: ${({ theme }) => theme.colors.grey};
+`;
+
+export const LikeButton = styled.TouchableOpacity`
+  margin-top: 16px;
+`;
+
+export const LikeButtonText = styled.Text``;
+
+export const Divider = styled.View`
+  margin: 16px 0;
+  background: ${({ theme }) => theme.colors.lightGrey};
+  height: 1px;
+  width: 100%;
+`;
+
+export const BirdInfoContainer = styled.View``;
+
+export const BirdInfo = styled.View`
+  margin-bottom: 16px;
+`;
+
+export const Label = styled.Text`
+  font-family: 'Roboto_700Bold';
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.black};
+  margin-bottom: 8px;
+`;
+
+export const InfoText = styled.Text`
+  font-family: 'Roboto_400Regular';
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.grey};
 `;
