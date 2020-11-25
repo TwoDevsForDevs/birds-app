c0e55454a39c4081b52bbb6850fa72586c3b4d15
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-25 20:52:57 -0300

    feat: create web struct with create-next-app

---
+++ packages/web/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../styles/GlobalStyles';
+import theme from '../styles/theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
---
+++ packages/web/src/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: '#7D40E7',
+    black: '#444444',
+    lightBlack: '#666666',
+    grey: '#999999',
+    lightGrey: '#CCCCCC',
+    white: '#FFFFFF',
+    background: '#F5F5F5',
+    inputBorder: '#DDDDDD',
+    error: '#DE3B3B'
+  },
+  radius: {
+    default: '8px',
+    small: '4px'
+  }
+} as const;
