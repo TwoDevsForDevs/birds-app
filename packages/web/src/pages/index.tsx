c4593a63b8abc1cef2817f4c04137182ac193fd3
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-28 10:11:22 -0300

    feat: create SignIn page

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Input from '../components/Input';
+
+import { Container, Form, Button } from '../styles/pages/SignIn';
 
 const Home: React.FC = () => {
-  return <h1>Hello World</h1>;
+  const formRef = useRef(null);
+
+  return (
+    <Container>
+      <Form ref={formRef} onSubmit={() => console.log()}>
+        <Input name="email" label="E-mail" placeholder="exemplo@email.com" />
+        <Input name="password" label="Senha" placeholder="*************" />
+
+        <Button type="submit">Entrar no sistema</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Home;
