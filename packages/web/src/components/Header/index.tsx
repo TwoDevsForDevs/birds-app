7dead759390aaf65544dccf08e7e9cc9d22a7885
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-13 00:24:13 -0300

    feat(web): create Header component

---
+++ packages/web/src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../hooks';
+
+import { Container, Content, Profile } from './styles';
+
+const Header: React.FC = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <NavLink to="/birds" activeClassName="selected">
+            PÁSSAROS
+          </NavLink>
+          <NavLink to="/registers" activeClassName="selected">
+            REGISTROS
+          </NavLink>
+        </nav>
+
+        <Profile>
+          <strong>{user.name}</strong>
+          <button type="button" onClick={signOut}>
+            sair do sistema
+          </button>
+        </Profile>
+      </Content>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ packages/web/src/components/Header/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  min-height: 64px;
+  background: ${({ theme }) => theme.colors.white};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGrey};
+
+  display: flex;
+`;
+
+export const Content = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    a {
+      color: ${({ theme }) => theme.colors.grey};
+      font-size: 15px;
+      font-weight: bold;
+
+      & + a {
+        margin-left: 24px;
+      }
+
+      &.selected {
+        color: ${({ theme }) => theme.colors.black};
+      }
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  strong {
+    margin-bottom: 5px;
+  }
+
+  button {
+    color: ${({ theme }) => theme.colors.error};
+  }
+`;
