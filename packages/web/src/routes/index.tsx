d5f0fbaf2e489ed68cd5c649efd70d8e26bd7bde
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-13 00:05:50 -0300

    fix(web): remove comments

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -4,16 +4,11 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '../pages/SignIn';
-// import { BirdsList, BirdsForm } from '../pages/Birds';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-
-      {/* <Route path="/birds" exact component={BirdsList} />
-      <Route path="/birds/new" component={BirdsForm} />
-      <Route path="/birds/edit/:id" component={BirdsForm} /> */}
     </Switch>
   );
 };
