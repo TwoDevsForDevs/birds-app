67b7510c1c9badfe3074bb5c4ca8413a0cd07e2e
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-19 00:12:59 -0300

    feat(web): add icon and color prop to button

--- packages/web/src/components/Form/Button/index.tsx
+++ packages/web/src/components/Form/Button/index.tsx
@@ -1,4 +1,6 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, ComponentType } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useTheme } from 'styled-components';
 
 import Spinner from '../../Spinner';
 
@@ -6,18 +8,31 @@ import { Container } from './styles';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   background?: string;
+  color?: string;
+  icon?: ComponentType<IconBaseProps>;
   loading?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
   background,
+  color,
+  icon: Icon,
   loading,
   children,
   ...rest
 }) => {
+  const { colors } = useTheme();
+
   return (
-    <Container background={background} {...rest}>
-      {loading ? <Spinner /> : children}
+    <Container background={background} color={color} hasIcon={!!Icon} {...rest}>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {Icon && <Icon size={20} color={colors.white} />}
+          {children}
+        </>
+      )}
     </Container>
   );
 };
--- packages/web/src/components/Form/Button/styles.ts
+++ packages/web/src/components/Form/Button/styles.ts
@@ -4,17 +4,19 @@ import { darken } from 'polished';
 interface ContainerProps {
   background?: string;
   color?: string;
+  hasIcon?: boolean;
 }
 
 export const Container = styled.button<ContainerProps>`
   height: 36px;
   width: 100%;
   background: ${({ theme, background }) => background || theme.colors.primary};
-  color: ${({ theme, background }) =>
-    background ? theme.colors.primary : theme.colors.white};
+  color: ${({ theme, color }) => color || theme.colors.white};
   border-radius: ${({ theme }) => theme.radius.small};
   font-size: 16px;
   font-weight: bold;
+  position: relative;
+  padding-left: ${props => props.hasIcon && 40}px !important;
   transition: background 0.2s;
 
   display: flex;
@@ -24,5 +26,12 @@ export const Container = styled.button<ContainerProps>`
   &:hover {
     background: ${({ theme, background }) =>
       darken(0.03, background || theme.colors.primary)};
+  }
+
+  svg {
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
   }
 `;
