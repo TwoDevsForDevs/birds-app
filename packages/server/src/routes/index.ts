2e1018835adb4e6f309b96e9ac49063fa680c780
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-22 13:26:11 -0300

    feat(server): add dietsRouter to root routes

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -5,6 +5,7 @@ import authMiddleware from '../modules/users/middlewares/authMiddleware';
 import usersRouter from '../modules/users/routes/users.routes';
 import sessionsRouter from '../modules/users/routes/sessions.routes';
 import profileRouter from '../modules/users/routes/profile.routes';
+import dietsRouter from '../modules/diets/routes/diets.routes';
 import birdsRouter from '../modules/birds/routes/birds.routes';
 import birdsRegisterRouter from '../modules/birdsRegisters/routes/birdsRegisters.routes';
 
@@ -18,6 +19,7 @@ routes.use(authMiddleware);
 
 routes.use('/profile', profileRouter);
 
+routes.use('/diets', dietsRouter);
 routes.use('/birds', birdsRouter);
 routes.use('/birds-registers', birdsRegisterRouter);
 
