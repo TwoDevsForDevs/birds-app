6f818cc6b9df59576119bfc9ea5818770a5150ac
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-22 19:02:15 -0300

    feat(web): create diets list and from pages

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -5,15 +5,23 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import { BirdsList, BirdsForm } from '../pages/Birds';
+import { DietsList, DietsForm } from '../pages/Diets';
 
 const Routes: React.FC = () => {
+  const birdsPath = '/birds';
+  const dietsPath = '/diets';
+
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
 
-      <Route path="/birds" exact component={BirdsList} isPrivate />
-      <Route path="/birds/new" component={BirdsForm} isPrivate />
-      <Route path="/birds/edit/:id" component={BirdsForm} isPrivate />
+      <Route path={`${birdsPath}`} exact component={BirdsList} isPrivate />
+      <Route path={`${birdsPath}/new`} component={BirdsForm} isPrivate />
+      <Route path={`${birdsPath}/edit/:id`} component={BirdsForm} isPrivate />
+
+      <Route path={`${dietsPath}`} exact component={DietsList} isPrivate />
+      <Route path={`${dietsPath}/new`} component={DietsForm} isPrivate />
+      <Route path={`${dietsPath}/edit/:id`} component={DietsForm} isPrivate />
     </Switch>
   );
 };
