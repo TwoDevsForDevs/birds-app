1aa89b6aed4cd3df98758d4309fb7af0111b508a
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-31 10:33:43 -0300

    feat(web): export all utils function from index

--- packages/web/src/utils/getValidationErrors.ts
+++ packages/web/src/utils/getValidationErrors.ts
@@ -4,10 +4,10 @@ interface Errors {
   [key: string]: string;
 }
 
-export default function getValidationErrors(err: ValidationError): Errors {
+export default function getVaidationErrors(err: ValidationError): Errors {
   const validationErrors: Errors = {};
 
-  err.inner.forEach(error => {
+  err.inner.forEach((error: any) => {
     validationErrors[error.path] = error.message;
   });
 
